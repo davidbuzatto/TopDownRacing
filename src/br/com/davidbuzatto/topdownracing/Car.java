4796b66df7356ad6fd526046e741dacd42c8d7d5
David Buzatto <davidbuzatto@gmail.com>
2025-02-21 18:03:37 -0300

    cpu cars

--- src/br/com/davidbuzatto/topdownracing/Car.java
+++ src/br/com/davidbuzatto/topdownracing/Car.java
@@ -1,7 +1,9 @@
 package br.com.davidbuzatto.topdownracing;
 
+import br.com.davidbuzatto.jsge.collision.CollisionUtils;
 import br.com.davidbuzatto.jsge.core.engine.EngineFrame;
 import br.com.davidbuzatto.jsge.image.Image;
+import br.com.davidbuzatto.jsge.math.MathUtils;
 import br.com.davidbuzatto.jsge.math.Vector2;
 
 /**
@@ -11,8 +13,9 @@ import br.com.davidbuzatto.jsge.math.Vector2;
  */
 public class Car {
     
-    private static final double ANGLE_SPEED = 100;
-    private static final double MAX_SPEED = 800;
+    private static final double ANGLE_SPEED = 200;
+    //private static final double MAX_SPEED = 800;
+    private static final double MAX_SPEED = 600;
     private static final double MOVE_ACCL = 600;
     
     private Vector2 pos;
@@ -22,43 +25,81 @@ public class Car {
     private double speed;
     private Image image;
     
+    private Vector2[] targets;
+    private int currentTarget;
+    
+    private boolean controlled;
     private EngineFrame e;
     
-    public Car( double x, double y, Image image, EngineFrame e ) {
+    public Car( double x, double y, Image image, boolean controlled, EngineFrame e ) {
         this.pos = new Vector2( x, y );
         this.image = image;
         this.dim = new Vector2( image.getWidth(), image.getHeight() );
         this.vel = new Vector2();
         this.rotation = -90;
+        this.targets = Main.createTargets();
+        this.controlled = controlled;
         this.e = e;
     }
     
     public void update( double delta ) {
         
-        if ( e.isKeyDown( EngineFrame.KEY_RIGHT ) ) {
-            rotation += delta * ANGLE_SPEED;
-        }
-        
-        if ( e.isKeyDown( EngineFrame.KEY_LEFT ) ) {
-            rotation -= delta * ANGLE_SPEED;
-        }
-        
         boolean moving = false;
+        double distance = pos.distanceSquare( targets[currentTarget] );
+        double changeTargetDistance = 10000;
         
-        if ( e.isKeyDown( EngineFrame.KEY_UP ) ) {
-            speed += MOVE_ACCL * delta;
+        if ( controlled ) {
+            
+            if ( e.isKeyDown( EngineFrame.KEY_RIGHT ) ) {
+                rotation += delta * ANGLE_SPEED;
+            }
+
+            if ( e.isKeyDown( EngineFrame.KEY_LEFT ) ) {
+                rotation -= delta * ANGLE_SPEED;
+            }
+
+            if ( e.isKeyDown( EngineFrame.KEY_UP ) ) {
+                speed += MOVE_ACCL * delta;
+                moving = true;
+                if ( speed > MAX_SPEED ) {
+                    speed = MAX_SPEED;
+                }
+            } else if ( e.isKeyDown( EngineFrame.KEY_DOWN ) ) {
+                speed -= MOVE_ACCL * delta;
+                moving = true;
+                if ( speed < -MAX_SPEED ) {
+                    speed = -MAX_SPEED;
+                }
+            }
+            
+        } else {
+            
+            Vector2 t = targets[currentTarget];
+            double angleToTarget = Math.atan2( pos.y - t.y, pos.x - t.x );
+            double beta = rotation - Math.toDegrees( angleToTarget ) - 180;
+            
+            if ( Math.sin( Math.toRadians( beta ) ) < 0 ) {
+                rotation += 4;
+            } else {
+                rotation -= 4;
+            }
+            
+            if ( MathUtils.getRandomValue( 0, 100 ) > 30 ) {
+                speed += MOVE_ACCL * delta / 2;
+            } else {
+                speed -= MOVE_ACCL * delta / 2;
+            }
+            
             moving = true;
+            
             if ( speed > MAX_SPEED ) {
                 speed = MAX_SPEED;
             }
-        } else if ( e.isKeyDown( EngineFrame.KEY_DOWN ) ) {
-            speed -= MOVE_ACCL * delta;
-            moving = true;
-            if ( speed < -MAX_SPEED ) {
-                speed = -MAX_SPEED;
-            }
+                    
         }
         
+        
+            
         if ( !moving ) {
             if ( speed < 0 ) {
                 speed += MOVE_ACCL * delta;
@@ -79,10 +120,22 @@ public class Car {
         pos.x += vel.x * delta;
         pos.y += vel.y * delta;
         
+        if ( distance < changeTargetDistance ) {
+            currentTarget = ( currentTarget + 1 ) % targets.length;
+            if ( currentTarget == 0 ) {
+                targets = Main.createTargets();
+            }
+        }
+        
     }
     
     public void draw() {
-        e.drawImage( image, pos.x, pos.y, dim.x / 2, dim.y / 2, rotation );
+        
+        e.drawImage( image, pos.x - dim.x / 2, pos.y - dim.y / 2, dim.x / 2, dim.y / 2, rotation );
+        
+        //e.fillCircle( pos, 50, EngineFrame.WHITE );
+        //e.drawLine( pos, targets[currentTarget], EngineFrame.WHITE );
+        
     }
     
     public double getCenterX() {
@@ -91,6 +144,23 @@ public class Car {
     
     public double getCenterY() {
         return pos.y + dim.y / 2;
+    }
+    
+    public double getX() {
+        return pos.x;
+    }
+    
+    public double getY() {
+        return pos.y;
+    }
+    
+    public void setPos( double x, double y ) {
+        pos.x = x;
+        pos.y = y;
+    }
+    
+    public boolean checkCollision( Car car ) {
+        return CollisionUtils.checkCollisionCircles( pos, 50, car.pos, 50 );
     }
     
 }
--- src/br/com/davidbuzatto/topdownracing/Main.java
+++ src/br/com/davidbuzatto/topdownracing/Main.java
@@ -3,6 +3,10 @@ package br.com.davidbuzatto.topdownracing;
 import br.com.davidbuzatto.jsge.core.Camera2D;
 import br.com.davidbuzatto.jsge.core.engine.EngineFrame;
 import br.com.davidbuzatto.jsge.image.Image;
+import br.com.davidbuzatto.jsge.math.MathUtils;
+import br.com.davidbuzatto.jsge.math.Vector2;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Top-Down Racing game main class.
@@ -11,7 +15,9 @@ import br.com.davidbuzatto.jsge.image.Image;
  */
 public class Main extends EngineFrame {
     
-    private Car car;
+    private Car mainCar;
+    private List<Car> cars;
+    
     private Image trackImage;
     private Camera2D camera;
     
@@ -21,15 +27,38 @@ public class Main extends EngineFrame {
     
     @Override
     public void create() {
+        
         setDefaultFontSize( 20 );
-        car = new Car( 330, 1000, loadImage( "resources/images/car01.png" ), this );
+        
+        mainCar = new Car( 390, 1000, loadImage( "resources/images/car01.png" ).resize( 0.5 ), true, this );
+        
+        cars = new ArrayList<>();
+        cars.add( new Car( 270, 1000, loadImage( "resources/images/car02.png" ).resize( 0.5 ), false, this ) );
+        cars.add( new Car( 330, 1000, loadImage( "resources/images/car03.png" ).resize( 0.5 ), false, this ) );
+        cars.add( new Car( 450, 1000, loadImage( "resources/images/car04.png" ).resize( 0.5 ), false, this ) );
+        cars.add(  mainCar );
+        
         trackImage = loadImage( "resources/images/track.png" );
+        
         camera = new Camera2D();
+        camera.zoom = 1;
+        
     }
     
     @Override
     public void update( double delta ) {
-        car.update( delta );
+        
+        for ( Car c : cars ) {
+            c.update( delta );
+        }
+        
+        resolveCarCollision();
+        updateCamera();
+        
+    }
+    
+    public void updateCamera() {
+        
         if ( isKeyDown( KEY_PAGE_UP ) ) {
             camera.zoom += 0.01;
         } else if ( isKeyDown( KEY_PAGE_DOWN ) ) {
@@ -38,25 +67,21 @@ public class Main extends EngineFrame {
                 camera.zoom = 0.1;
             }
         }
-        updateCamera();
-    }
-    
-    public void updateCamera() {
         
-        if ( car.getCenterX() <= getScreenWidth() / 2 ) {
+        if ( mainCar.getCenterX() <= getScreenWidth() / 2 ) {
             camera.target.x = getScreenWidth() / 2;
-        } else if ( car.getCenterX() >= trackImage.getWidth() - getScreenWidth() / 2 ) {
+        } else if ( mainCar.getCenterX() >= trackImage.getWidth() - getScreenWidth() / 2 ) {
             camera.target.x = trackImage.getWidth() - getScreenWidth() / 2 ;
         } else {
-            camera.target.x = car.getCenterX();
+            camera.target.x = mainCar.getCenterX();
         }
         
-        if ( car.getCenterY() <= getScreenHeight() / 2 ) {
+        if ( mainCar.getCenterY() <= getScreenHeight() / 2 ) {
             camera.target.y = getScreenHeight() / 2;
-        } else if ( car.getCenterY() >= trackImage.getHeight() - getScreenHeight() / 2 ) {
+        } else if ( mainCar.getCenterY() >= trackImage.getHeight() - getScreenHeight() / 2 ) {
             camera.target.y = trackImage.getHeight() - getScreenHeight() / 2 ;
         } else {
-            camera.target.y = car.getCenterY();
+            camera.target.y = mainCar.getCenterY();
         }
         
         camera.offset.x = getScreenWidth() / 2;
@@ -66,12 +91,48 @@ public class Main extends EngineFrame {
     
     @Override
     public void draw() {
+        
         clearBackground( WHITE );
+        
         beginMode2D( camera );
+        
         drawImage( trackImage, 0, 0 );
-        car.draw();
+        for ( Car c : cars ) {
+            c.draw();
+        }
+        
         endMode2D();
+        
         drawFPS( 20, 20 );
+        
+    }
+    
+    private void resolveCarCollision() {
+        
+        for ( int i = 0; i < cars.size(); i++ ) {
+            for ( int j = i+1; j < cars.size(); j++ ) {
+                Car c1 = cars.get( i );
+                Car c2 = cars.get( j );
+                if ( c1.checkCollision( c2 ) ) {
+                    double a = Math.atan2( c1.getCenterY() - c2.getCenterY(), c1.getCenterX() - c2.getCenterX() );
+                    c1.setPos( c1.getX() + 2 * Math.cos( a ), c1.getY() + 2 * Math.sin( a ) );
+                    c2.setPos( c2.getX() + 2 * Math.cos( a + Math.PI ), c2.getY() + 2 * Math.sin( a + Math.PI ) );
+                }
+            }
+        }
+        
+    }
+    
+    public static Vector2[] createTargets() {
+        int rMin = -100;
+        int rMax = 100;
+        return new Vector2[]{
+            new Vector2( 400 + MathUtils.getRandomValue( rMin, rMax ), 400 + MathUtils.getRandomValue( rMin, rMax ) ),
+            new Vector2( 1650 + MathUtils.getRandomValue( rMin, rMax ), 400 + MathUtils.getRandomValue( rMin, rMax ) ),
+            new Vector2( 1650 + MathUtils.getRandomValue( rMin, rMax ), 1650 + MathUtils.getRandomValue( rMin, rMax ) ),
+            new Vector2( 400 + MathUtils.getRandomValue( rMin, rMax ), 1650 + MathUtils.getRandomValue( rMin, rMax ) ),
+            new Vector2( 400 + MathUtils.getRandomValue( rMin, rMax ), 1000 ),
+        };
     }
     
     public static void main( String[] args ) {
